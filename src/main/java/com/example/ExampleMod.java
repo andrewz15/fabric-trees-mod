fa3e45f743069c8a920680fdddb64ed2f9667d4f
Andrew Zhang <az@MacBook-Pro-5.attlocal.net>
2026-06-06 14:58:32 -0700

    Remove unused code.

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -26,8 +26,6 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 /*
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             BlockPos p = player.getOnPos();
